c7686f680eab0d77da551a98a9482bfbb0083157
Michael Morris <michaelmorris121@yahoo.com>
2026-01-20 11:41:24 -0600

    add base stylesheet and global styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import './styles.css';
-import {addProject, getProjects, loadApp} from './app.js';
+import {loadApp} from './app.js';
 import {render} from './ui.js';
 
 loadApp();
 render();
-
-if (getProjects().length === 0) {
-    addProject('Default');
-}
