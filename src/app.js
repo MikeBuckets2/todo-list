129a5d70030e12620d6a317f7f4204016df474ff
Michael Morris <michaelmorris121@yahoo.com>
2026-01-20 10:14:34 -0600

    restore app state from local storage on load

--- src/app.js
+++ src/app.js
@@ -1,17 +1,47 @@
 import {createProject} from './project.js';
+import {createTodo} from './todo.js';
+import {saveProjects, loadProjects} from './storage.js';
 
 const projects = [];
+
+function loadApp() {
+    const stored = loadProjects();
+
+    stored.forEach(projectData => {
+        const project = createProject(projectData.name);
+
+        projectData.todos.forEach(todoData => {
+            const todo = createTodo(
+                todoData.title, 
+                todoData.description, 
+                todoData.dueDate, 
+                todoData.priority, 
+                todoData.notes, 
+                todoData.checklist
+            );
+            todo.completed = todoData.completed;
+            project.addTodo(todo);
+        });
+        
+        projects.push(project);
+    });
+
+    if (projects.length === 0) {
+        addProject('Default');
+    };
+};
+
+function saveApp() {
+    saveProjects(projects);
+};
+
+function addProject(name) {
+    projects.push(createProject(name));
+    saveApp();
+};
 
 function getProjects() {
     return projects;
 };
 
-function addProject(name) {
-    projects.push(createProject(name));
-};
-
-function getProject(index) {
-    return projects[index];
-};
-
-export {getProjects, addProject, getProject};
+export {loadApp, saveApp, getProjects, addProject};
